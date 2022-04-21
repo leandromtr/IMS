19066687dfce0c200455ba970a92a92d87847bf2
Leandro Reis <leandro.reis@nextbitt.com>
2022-04-21 10:58:10 +0100

    Add ViewInventories use case

---
+++ IMS.Plugins.EFCore/InventoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.Plugins.EFCore
+{
+    public class InventoryRepository : IInventoryRepository
+    {
+        Task<IEnumerable<Inventory>> IInventoryRepository.GetInventoriesByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
